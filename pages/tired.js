1f90064230e5c2575daa359ef69c2a136a8f096e
Anton Andersson <anton.andersson@brightstraining.com>
2023-03-28 10:37:13 +0200

    changed bg-colors

--- pages/tired.js
+++ pages/tired.js
@@ -1,11 +1,12 @@
 import styles from "@/styles/Tired.module.css";
+import Layout from "./components/Layout";
 
 export default function Tired() {
   return (
-    <>
+    <Layout backgroundColor="#181d31" color="#fffefe">
       <div>
         <p>HELLO TIRED!</p>
       </div>
-    </>
+    </Layout>
   );
 }

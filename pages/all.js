905786571e8a78cabab38790fbaa966bf4f4b42d
unknown <daniela.alwan@digitalent.se>
2023-04-03 14:24:23 +0200

    added style to lugna

--- pages/all.js
+++ pages/all.js
@@ -30,7 +30,7 @@ export default function All({ moods }) {
           >
             <circle cx="26" cy="26" r="26" fill="#F9DFA1" />
           </svg>
-          ALL EXERCISES
+          npm install lru-cache ALL EXERCISES
         </h1>
         <div className={styles.descriptionContainer}>
           <h3 className={styles.webDesc}>
